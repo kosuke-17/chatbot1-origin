d6708c575206424d8b44095215f767c207d5e95c
kosuke-17 <tamutamu17@icloud.com>
2021-09-07 12:06:39 +0900

    トップページを初期化

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+const App = () => {
+  const [answers, setAnswers] = useState([]);
+  const [chats, setChats] = useState([]);
+  const [currentId, setCurrentId] = useState("init");
+  const [dataset, setDataset] = useState({});
+  const [open, setOpen] = useState(false);
+  
+  return (
+    <>
+    テスト
+    </>
+  );
+}
+
+export default App;
