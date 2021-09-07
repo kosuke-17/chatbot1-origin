4c7d1a4d07afba68454766b9ed0c90e54c2052f8
kosuke-17 <tamutamu17@icloud.com>
2021-09-07 12:36:44 +0900

    firestoreを使う準備が完了

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyATsK39FFCkGLTZAKk2NYOnOHxw7eGK_-Y",
+  authDomain: "chatbot1-origin.firebaseapp.com",
+  projectId: "chatbot1-origin",
+  storageBucket: "chatbot1-origin.appspot.com",
+  messagingSenderId: "343172931280",
+  appId: "1:343172931280:web:96678435fb5edcbca808d5",
+  measurementId: "G-RBRFKY1FGW"
+};
+// firestoreのrulesを設定しているからこの値はgithubに公開しても問題ない
+
+export default firebaseConfig
