ed7a1278fb72eaad415879cc44c1d25517915019
kosuke-17 <tamutamu17@icloud.com>
2021-09-07 12:06:22 +0900

    firebaseにデータを保存する処理を作成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,28 @@
-// import * as functions from "firebase-functions";
+// パッケージをそれぞれfunctionsとadminにimport
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// initializeApp()で初期化
+admin.initializeApp();
+// admin権限でfirestoreを操作できるように定数dbに入れておく
+const db = admin.firestore();
+
+const sendResponse = (response: functions.Response, statusCode: number, body:any) => {
+  response.send({
+    statusCode,
+    body: JSON.stringify(body)
+});
+}
+
+export const addDataset = functions.https.onRequest(async (req: any, res: any) => {
+  if (req.method !== 'POST') {
+      sendResponse(res, 405, {error: "Invalid Request"})
+  } else {
+      const dataset = req.body;
+      for (const key of Object.keys(dataset)) {
+          const data = dataset[key];
+          await db.collection('questions').doc(key).set(data)
+      }
+      sendResponse(res, 200, {message: 'データの保存が完了しました。'});
+  }
+});
